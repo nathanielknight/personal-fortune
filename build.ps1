2c66a469316eaa1a61afb834411b07540daae02f
Nathaniel Knight <nathaniel.ep@gmail.com>
2020-06-10 16:29:45 -0700

    Make build container suitable for testing

--- build.ps1
+++ build.ps1
@@ -12,17 +12,41 @@ The container is for building `personal-fortune` on non-Linux hosts.
 #>
 function Create-BuildContainer {
     $dirpath = (Get-Location).ToString().Replace("\", "/")
-    docker build -t personal-fortune-build -f Dockerfile.build .
+    docker build -t personal-fortune-build -f Dockerfile.dev .
     docker create -v "${dirpath}:/root/personal-fortune" --name pfb personal-fortune-build
 }
 
 <#
 .SYNOPSIS
-Run the build container to 
+Run `cargo build` in the build container.
 .DESCRIPTION
 Creates a container with Cargo installed and the current location mounted.
 The container is for building `personal-fortune` on non-Linux hosts.
 #>
 function Build-PersonalFortune {
     docker start -ai pfb
+    cp .\target\x86_64-unknown-linux-gnu\release\personal-fortune .
 }
+
+<#
+.SYNOPSIS
+Create a temporray Docker container for running `personal-fortune`.
+#>
+function Invoke-DevContainer {
+    $dirpath = (Get-Location).ToString().Replace("\", "/")
+    docker run -it --rm `
+        -v "${dirpath}:/root/personal-fortune" `
+        --publish "6429:6429" `
+        --name pfdev `
+        personal-fortune-build /bin/bash
+}
+<#
+.SYNOPSIS
+Create the CapRover tarfile, ready for deployment.
+.DESCRIPTION
+Creates a TarFile suitable for deployment to a CapRover server.
+#>
+function Build-TarFile {
+    tar -cf personal-fortune.tar personal-fortune fortunes.sqlite Dockerfile.serve
+}
+
