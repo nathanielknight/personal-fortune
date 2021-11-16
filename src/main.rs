0efe3930c52ac6cad402ce543a0ce4844fcc6bc4
Nathaniel Knight <nathaniel.ep@gmail.com>
2021-11-15 16:01:24 -0800

    Migrate from SimpleServer to Tide

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
+use anyhow;
 use std::env;
-
-use rusqlite;
-use simple_server::{Request, ResponseBuilder, ResponseResult, Server};
 
 mod model;
 
@@ -42,66 +40,54 @@ fn in_site_template(body: &str) -> String {
     )
 }
 
-fn abort(code: u16, mut rb: ResponseBuilder) -> ResponseResult {
-    Ok(rb.status(code).body(vec![])?)
-}
-
-fn render_entry(entry: model::Entry, mut rb: ResponseBuilder) -> ResponseResult {
+fn render_entry(entry: model::Entry) -> tide::Response {
     let e_str: String = entry.into();
-    Ok(rb
-        .status(200)
+    tide::Response::builder(200)
         .header("content-type", "text/html; charset=UTF-8")
-        .body(in_site_template(&e_str).as_bytes().to_vec())?)
+        .body(in_site_template(&e_str).as_bytes().to_vec())
+        .build()
 }
 
-fn random_entry(rb: ResponseBuilder) -> ResponseResult {
+fn abort(status: tide::StatusCode, msg: &str) -> AppResponse {
+    let resp = tide::Response::builder(status)
+        .header("content-type", "text/plain; charset=UTF-8")
+        .body(msg)
+        .build();
+    Ok(resp)
+}
+
+async fn random_entry(_req: tide::Request<()>) -> AppResponse {
     match model::random_entry() {
-        Ok(entry) => render_entry(entry, rb),
-        Err(_) => abort(500, rb),
+        Ok(entry) => Ok(render_entry(entry)),
+        Err(_) => abort(tide::StatusCode::InternalServerError, "Failed to retrieve random entry."),
     }
 }
 
-fn entry(req: &Request<Vec<u8>>, rb: ResponseBuilder) -> ResponseResult {
-    let path = req.uri().path();
-    assert!(req.uri().path().starts_with("/entry/"));
-    let entry_id = &path[7..path.len()];
-    match model::get_entry(entry_id) {
-        Ok(e) => render_entry(e, rb),
-        Err(_) => return abort(404, rb),
+async fn entry(req: tide::Request<()>) -> AppResponse {
+    match req.param("slug") {
+        Ok(slug) => match model::get_entry(slug) {
+            Ok(e) => Ok(render_entry(e)),
+            Err(_) => abort(tide::StatusCode::NotFound, "No such entry."),
+        },
+        Err(_) => abort(tide::StatusCode::BadRequest, "usage: /entry/<entry slug>"),
     }
 }
 
-fn log_request(req: &Request<Vec<u8>>) {
-    let method = req.method();
-    let path = req.uri().path();
-    println!("{} {}", method, path);
-}
+type AppResponse = tide::Result<tide::Response>;
 
-fn handler(request: Request<Vec<u8>>, responsebuilder: ResponseBuilder) -> ResponseResult {
-    log_request(&request);
-    let path = request.uri().path();
-    if path == "/" {
-        random_entry(responsebuilder)
-    } else if path.starts_with("/entry/") {
-        entry(&request, responsebuilder)
-    } else {
-        abort(404, responsebuilder)
-    }
-}
-
-fn main() -> Result<(), rusqlite::Error> {
-    use std::path;
-    println!("Starting Personal Fortune Server");
+#[async_std::main]
+async fn main() -> Result<(), anyhow::Error> {
+    println!("Initializing");
+    model::init_db()?;
+    let mut app = tide::new();
+    app.at("").get(random_entry);
+    app.at("entry/:slug").get(entry);
 
     println!("Loading configuration");
-    model::init_db()?;
     let host = env::var("WTIIRN_HOST").unwrap_or_else(|_| "0.0.0.0".to_string());
     let port = env::var("PORT").unwrap_or_else(|_| "6429".to_string());
 
-    println!("Initializing server");
-    let mut server = Server::new(handler);
-    server.set_static_directory(path::Path::new("static"));
-
     println!("Serving personal-fortune on {}:{}", host, port);
-    server.listen(&host, &port);
+    app.listen(format!("{}:{}", host, port)).await?;
+    Ok(())
 }
