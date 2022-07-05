74a1198b2bb560e1ecaabbc1a9112bdf26a10081
Nathaniel Knight <nathaniel.ep@gmail.com>
2022-07-05 15:46:10 -0700

    Update dependencies

--- src/main.rs
+++ src/main.rs
@@ -5,23 +5,31 @@ use tide::convert::Deserialize;
 mod model;
 mod views;
 
-fn render_entry(entry: model::Entry) -> tide::Response {
-    tide::Response::builder(200)
-        .header("content-type", "text/html; charset=UTF-8")
-        .body(views::quote(&entry).as_bytes().to_vec())
-        .build()
+#[async_std::main]
+async fn main() -> Result<(), anyhow::Error> {
+    pretty_env_logger::init();
+    log::info!("Initializing");
+    let mut conn = db_connection()?;
+    model::init_db(&mut conn)?;
+    let mut app = tide::new();
+    app.at("").get(random_entry);
+    app.at("entry/:slug").get(entry);
+    app.at("search").get(search);
+
+    log::info!("Loading configuration");
+    let host = env::var("WTIIRN_HOST").unwrap_or_else(|_| "0.0.0.0".to_string());
+    let port = env::var("PORT").unwrap_or_else(|_| "6429".to_string());
+
+    app.listen(format!("{}:{}", host, port)).await?;
+    Ok(())
 }
 
-fn abort(status: tide::StatusCode, msg: &str) -> AppResponse {
-    let resp = tide::Response::builder(status)
-        .header("content-type", "text/plain; charset=UTF-8")
-        .body(msg)
-        .build();
-    Ok(resp)
-}
+// -------------------------------------------------------------------------
+// Handlers
 
 async fn random_entry(_req: tide::Request<()>) -> AppResponse {
-    match model::random_entry() {
+    let mut conn = db_connection()?;
+    match model::random_entry(&mut conn) {
         Ok(entry) => Ok(render_entry(entry)),
         Err(_) => abort(
             tide::StatusCode::InternalServerError,
@@ -32,7 +40,8 @@ async fn random_entry(_req: tide::Request<()>) -> AppResponse {
 
 async fn entry(req: tide::Request<()>) -> AppResponse {
     let slug = req.param("slug")?;
-    match model::get_entry(slug) {
+    let mut conn = db_connection()?;
+    match model::get_entry(&mut conn, slug) {
         Ok(e) => Ok(render_entry(e)),
         Err(_) => abort(tide::StatusCode::NotFound, "No such entry."),
     }
@@ -54,10 +63,11 @@ struct SearchQuery {
 
 async fn search(req: tide::Request<()>) -> AppResponse {
     let searchquery: SearchQuery = req.query()?;
+    let mut conn = db_connection()?;
     log::debug!("searchquery = {:?}", searchquery);
     let query = searchquery.q;
     let results = match query {
-        Some(ref q) => model::search(q)?,
+        Some(ref q) => model::search(&mut conn, q)?,
         None => Vec::new(),
     };
     render_search(&query.unwrap_or_default(), &results)
@@ -65,20 +75,27 @@ async fn search(req: tide::Request<()>) -> AppResponse {
 
 type AppResponse = tide::Result<tide::Response>;
 
-#[async_std::main]
-async fn main() -> Result<(), anyhow::Error> {
-    pretty_env_logger::init();
-    log::info!("Initializing");
-    model::init_db()?;
-    let mut app = tide::new();
-    app.at("").get(random_entry);
-    app.at("entry/:slug").get(entry);
-    app.at("search").get(search);
+// -------------------------------------------------------------------------
+// Utils
+const DB_PATH: &str = "./fortunes.sqlite";
 
-    log::info!("Loading configuration");
-    let host = env::var("WTIIRN_HOST").unwrap_or_else(|_| "0.0.0.0".to_string());
-    let port = env::var("PORT").unwrap_or_else(|_| "6429".to_string());
+fn db_connection() -> Result<rusqlite::Connection, rusqlite::Error> {
+    use std::path;
+    let db_path = path::Path::new(DB_PATH);
+    rusqlite::Connection::open(&db_path)
+}
 
-    app.listen(format!("{}:{}", host, port)).await?;
-    Ok(())
+fn render_entry(entry: model::Entry) -> tide::Response {
+    tide::Response::builder(200)
+        .header("content-type", "text/html; charset=UTF-8")
+        .body(views::quote(&entry).as_bytes().to_vec())
+        .build()
+}
+
+fn abort(status: tide::StatusCode, msg: &str) -> AppResponse {
+    let resp = tide::Response::builder(status)
+        .header("content-type", "text/plain; charset=UTF-8")
+        .body(msg)
+        .build();
+    Ok(resp)
 }
